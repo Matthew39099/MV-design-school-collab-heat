fae5cc903b79e7119346a8ec574d3aba31b5a45c
Will <adamwd1@student.op.ac.nz>
2026-05-26 12:24:42 +1200

    feat: Main menu now loads game with a robust scene swap manager, audio options now has a master slider prepped for controlling all audio.

---
+++ heat-test/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private Slider volumeSlider;
+
+    private void Awake()
+    {
+        // Singleton setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        LoadVolume();
+    }
+
+    private void Start()
+    {
+        // Set slider to saved value
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        }
+    }
+
+    // Called by UI slider
+    public void SetMasterVolume(float sliderValue)
+    {
+        // Convert to logarithmic scale
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+
+        // Save volume
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+    }
+
+    private void LoadVolume()
+    {
+        float volume = PlayerPrefs.GetFloat("MasterVolume", 1f);
+
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+}

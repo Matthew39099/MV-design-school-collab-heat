988727811f5ccbf90061593e02c96443fd8fd54e
visma376 <visma376@student.otago.ac.nz>
2026-06-25 00:38:10 +1200

    Refactor: changed death logic to use new safe ground loction

--- heat-test/Assets/Scripts/DeathPitController.cs
+++ heat-test/Assets/Scripts/DeathPitController.cs
@@ -2,43 +2,19 @@ using UnityEngine;
 
 public class DeathPitController : MonoBehaviour
 {
-    private Transform spawnPoint;
     private SafeGroundSaver safeGroundSaver;
 
     private void Start()
     {
-        // Find the respawn point in the scene
-        GameObject respawnObject = GameObject.FindGameObjectWithTag("Respawn");
-        safeGroundSaver = GetComponent<SafeGroundSaver>();
-        if (respawnObject != null)
-        {
-            spawnPoint = respawnObject.transform;
-        }
-        else
-        {
-            Debug.LogError("No object with tag 'Respawn' found.");
-        }
+        safeGroundSaver = GameObject.FindGameObjectWithTag("Player").GetComponent<SafeGroundSaver>();
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
-            CharacterController controller = collision.GetComponent<CharacterController>();
-
-            if (controller != null && spawnPoint != null)
-            {
-                // Disable controller before teleporting
-                controller.enabled = false;
-
-                // Move player to respawn point
-                safeGroundSaver.WarpPlayerToSafeGround();
-
-                // Re-enable controller
-                controller.enabled = true;
-
-                Debug.Log("Respawn");
-            }
+            safeGroundSaver.WarpPlayerToSafeGround();
+            Debug.Log("Player respawned");
         }
     }
 }
